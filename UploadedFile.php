f8b8d928b5b79f815a99b87b1f9ef9164522310f
basic-app <dev@basic-app.com>
2021-06-14 18:38:41 +0300

    n/a

--- UploadedFile.php
+++ UploadedFile.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * @author basic-app <dev@basic-app.com>
+ * @license MIT
+ * @link https://basic-app.com
+ */
 namespace BasicApp\Test;
 
 class UploadedFile extends \CodeIgniter\HTTP\Files\UploadedFile
