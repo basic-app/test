1ba5667d5c79b73bf65445544846de843deaa4e3
basic-app <dev@basic-app.com>
2021-07-15 17:07:37 +0300

    n/a

---
+++ UnitTestCase.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author basic-app <dev@basic-app.com>
+ * @license MIT
+ * @link https://basic-app.com
+ */
+namespace BasicApp\Test;
+
+class UnitTestCase extends \CodeIgniter\Test\CIUnitTestCase
+{
+}
